e2076235e9f195f9bd7a2bd83102914dc7e29b13
Bruna <53488109+brunaboeger@users.noreply.github.com>
2024-08-29 13:40:44 -0300

    alterando a frase de destaque

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # clone-tabnews
 
 Projeto para aprender programação com base no curso.dev
+
+# setup
+
+```
+$ npm install
+
+```
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 function Home() {
-  const msg = <h1>Você é o amor da minha vida 💞</h1>;
-  return msg;
+  const img1 = <img src="/img-1.jpg" height="400px"></img>;
+  const img2 = <img src="/img-2.jpg" height="400px"></img>;
+  const img3 = <img src="/img-3.jpg" height="400px"></img>;
+  const msg = (
+    <h1 style={{ textAlign: "center" }}>Você é o amor da minha vida 💞</h1>
+  );
+  const mountMessage = (
+    <div>
+      {msg}
+      <div style={{ textAlign: "center" }}>
+        {img1}
+        {img2}
+        {img3}
+      </div>
+    </div>
+  );
+  return mountMessage;
 }
 
 export default Home;
