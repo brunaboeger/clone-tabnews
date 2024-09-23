bf4ef1c5b5fc937b1e64895acd46a4a149478a72
Bruna <53488109+brunaboeger@users.noreply.github.com>
2024-09-23 01:56:12 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
 
-  response
-    .status(200)
-    .json({ chave: "Será que os acentos funcionam? Sim? Não?" });
+  const dbVersionQuery = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionQuery.rows[0].server_version;
+
+  const dbMaxConnectionsQuery = await database.query("SHOW max_connections;");
+  const dbMaxConnections = parseInt(
+    dbMaxConnectionsQuery.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsQuery = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dbOpenedConnections = dbOpenedConnectionsQuery.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: dbVersion,
+        max_connections: dbMaxConnections,
+        opened_connections: parseInt(dbOpenedConnections),
+      },
+    },
+  });
 }
 
 export default status;
